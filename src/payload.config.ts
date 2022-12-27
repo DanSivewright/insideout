452f9a27a6b86080996164f8c8c51b9cb418a8dd
Dan Sivewright <dan@wixels.com>
2022-12-27 10:33:17 +0200

    Revert "restore collections"
    This reverts commit 6857f35cf9521cb545395195d1366b125a625ea1.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,44 +1,22 @@
-import { buildConfig } from "payload/config";
-import path from "path";
-import Users from "./collections/Users";
-import Companies from "./collections/Companies";
-import Media from "./collections/Media";
-import Mines from "./collections/Mines";
-import Procurements from "./collections/Procurements";
-import Sites from "./collections/Sites";
-import Menus from "./collections/Menus";
-import { Pages } from "./collections/Pages";
-import { Logo } from "./components/Logo";
-import { Icon } from "./components/Icon";
+import { buildConfig } from 'payload/config';
+import path from 'path';
+// import Examples from './collections/Examples';
+import Users from './collections/Users';
 
 export default buildConfig({
-  serverURL: process.env.PAYLOAD_PUBLIC_BASE_DNS,
-  rateLimit: {
-    trustProxy: true,
-  },
+  serverURL: 'http://localhost:3000',
   admin: {
     user: Users.slug,
-    components: {
-      graphics: {
-        Logo: Logo,
-        Icon: Icon,
-      },
-    },
   },
   collections: [
     Users,
-    Companies,
-    Media,
-    Mines,
-    Sites,
-    Procurements,
-    Menus,
-    Pages,
+    // Add Collections here
+    // Examples,
   ],
   typescript: {
-    outputFile: path.resolve(__dirname, "payload-types.ts"),
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
   graphQL: {
-    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
   },
 });
